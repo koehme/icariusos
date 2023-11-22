93ec09bcbde52f4f6f21702e6f65a7c2444c006d
Kevin Oehme <Kevin_Oehme@outlook.de>
2023-11-22 21:09:04 +0100

    Set up segments manually
    In this commit, the bootloader code is configured to start at memory address 0 and manually sets the data segment (ds), extra segment (es), and stack segment (ss) to 0x7C0. This adjustment ensures proper memory access and aligns with the common practice for x86 bootloaders. The manual configuration is implemented to address potential discrepancies in segment values introduced by certain computers or BIOS systems during the boot process.

--- src/boot.asm
+++ src/boot.asm
@@ -1,5 +1,21 @@
-ORG 0x7c00
+ORG 0                           ; Allows manual setting of segments to 0x7C00 later, addressing potential BIOS discrepancies in segment values during boot loading.
 BITS 16
+
+; Initializes the environment for the bootloader.
+; configuring segment registers and setting up the stack
+init:
+    cli                         ; Disable interrupts (cli) to ensure a stable environment during critical initialization,
+                                ; preventing interruptions that might interfere with the setup process
+    mov ax, 0x7c0               ; Set up data segment (ds) and extra segment (es)
+    mov ds, ax
+    mov es, ax
+    mov ax, 0x00                ; Set the stack segment (ss) to 0x00
+                                ; to initialize the stack at the bottom of the addressable memory space in the x86 real mode
+
+    mov ss, ax
+    mov sp, 0x7c00              ; Set the stack pointer (sp) to 0x7C00, which is the end of the bootloader code,
+                                ; to allow the stack to grow downward from this location in the x86 real mode
+    sti                         ; Enable interrupts
 
 start:
     mov si, message
