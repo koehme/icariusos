130004f442076a99987186b2af9129b387f527e9
Kevin Oehme <Kevin_Oehme@outlook.de>
2023-11-22 21:28:07 +0100

    Implement a custom divide by zero interrupt handler

--- src/boot.asm
+++ src/boot.asm
@@ -10,9 +10,15 @@ bpb:
     times 33 db 0    ; Additional space or padding, filled with null bytes
 
 ; Initial Jump to direct boot loader execution to a predictable location
-; for the cs segment and ensure a consistent environment for further boot code execution
+; for the CS segment and ensure a consistent environment for further boot code execution
 start:
     jmp 0x7c0:init
+
+isr_0:
+    mov ah, 0x0e
+    mov al, '0'
+    int 0x10
+    iret
 
 ; Initializes the environment for the bootloader.
 ; configuring segment registers and setting up the stack
@@ -22,7 +28,7 @@ init:
     mov ax, 0x7c0               ; Set up data segment (ds) and extra segment (es)
     mov ds, ax
     mov es, ax
-    mov ax, 0x00                ; Set the stack segment (ss) to 0x00
+    mov ax, 0x00                ; Set the stack segment (SS) to 0x00
                                 ; to initialize the stack at the bottom of the addressable memory space in the x86 real mode
 
     mov ss, ax
@@ -30,8 +36,19 @@ init:
                                 ; to allow the stack to grow downward from this location in the x86 real mode
     sti                         ; Enable interrupts
 
+    mov ax, 0x00                ; Set the DS temporarily to the appropiate 0x00 starting address for the isr
+    mov ds, ax                  ; Who our first interrupt service handler will be placed
+
+    mov word[ds:0x00], isr_0    ; Set the first ISR to custom ISR isr_0 at address 0x0000:0x0000 (Instruction Pointer)
+    mov word[ds:0x02], 0x7c0    ; Set the CS (Code Segment) to 0x7c0 because (0x7c0 << 4) + isr_0 or (0x7c0 * 16) + isr_0 = physical address of isr_0
+
+    int 0                       ; call interrupt isr_0
+
+    mov ax, 0x7c0               ; Restore the original DS value for correct data segment mapping
+    mov ds, ax
+
 ; This is a print routine designed for testing purposes
-; It prints a null-terminated string pointed to by si
+; It prints a null-terminated string pointed to by SI
 print_message:
     mov si, message
     call print
