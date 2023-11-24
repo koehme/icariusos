953917b5417742bd77c7974ad79bec03bb61fe3d
Kevin Oehme <Kevin_Oehme@outlook.de>
2023-11-24 21:24:04 +0100

    Add new comments

--- src/bootloader/loader.asm
+++ src/bootloader/loader.asm
@@ -2,15 +2,24 @@
 global _start
 CODE_SEG equ 0x08
 DATA_SEG equ 0x10
-
+;=============================================================================
+; Start Initialization
+;
+; This section sets up segment registers, initializes the stack pointer and 
+; enables the fast A20 line for accessing all memory.
+;
+; @param None
+;
+; @return None
+;=============================================================================
 _start:
-    mov ax, DATA_SEG
+    mov ax, DATA_SEG            ; Set up data segment registers
     mov ss, ax
     mov ds, ax
     mov es, ax
     mov fs, ax
     mov gs, ax
-    mov ebp, 0x00200000
+    mov ebp, 0x00200000         ; Set base pointer (ebp) and stack pointer (esp)
     mov esp, ebp
 ; Enable the fast a20 line, so that all memory can be accessed
 .enable_a20:
@@ -18,4 +27,4 @@ _start:
     or al, 2
     out 0x92, al
 
-    jmp $
+    jmp $                       ; Infinite loop to halt execution at this point
