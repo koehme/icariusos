328fc4d9520336da24c8413d1c34f94785ccdcf8
Kevin Oehme <Kevin_Oehme@outlook.de>
2023-11-27 21:15:28 +0100

    Initialization of the operating system kernel with VGA display
    Welcome messages have been added.

--- src/kernel/kernel.c
+++ src/kernel/kernel.c
@@ -1,3 +1,19 @@
-#include "kernel.h"
+/**
+ * @file kernel.c
+ * @author Kevin Oehme
+ * @copyright MIT
+ */
 
-void kmain(void){};
+#include "kernel.h"
+#include "vga.h"
+
+void kmain(void)
+{
+    VgaDisplay display;
+    vga_display_init(&display, (uint16_t *)0xb8000, 80, 25);
+    vga_display_clear(&display);
+
+    vga_print(&display, "Hello World!\n");
+    vga_print(&display, "Icarius Kernel!");
+    return;
+};
