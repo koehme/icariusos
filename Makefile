610af5dc33bcaf188663685483ef1537f456bc34
Kevin Oehme <Kevin_Oehme@outlook.de>
2023-11-29 21:05:11 +0100

    Refactoring

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
-DEPENDENCIES = ./obj/loader.o ./obj/kernel.o ./obj/vga.o ./obj/mem.o ./obj/idtloader.o ./obj/idt.o
+DEPENDENCIES = ./obj/pm.o ./obj/kernel.o ./obj/vga.o ./obj/mem.o ./obj/idt.asm.o ./obj/idt.o
 INCLUDES = -I./src/include
 FLAGS = -g -ffreestanding -falign-jumps -falign-functions -falign-labels -falign-loops -fstrength-reduce -fomit-frame-pointer -finline-functions -Wno-unused-function -fno-builtin -Werror -Wno-unused-label -Wno-cpp -Wno-unused-parameter -nostdlib -nostartfiles -nodefaultlibs -Wall -O0
 
@@ -15,8 +15,8 @@ all: ./bin/boot.bin ./bin/kernel.bin
 ./bin/boot.bin: ./src/bootloader/boot.asm
 	nasm -f bin ./src/bootloader/boot.asm -o ./bin/boot.bin
 
-./obj/loader.o: ./src/bootloader/loader.asm
-	nasm -f elf -g ./src/bootloader/loader.asm -o ./obj/loader.o
+./obj/pm.o: ./src/bootloader/pm.asm
+	nasm -f elf -g ./src/bootloader/pm.asm -o ./obj/pm.o
 
 ./obj/kernel.o: ./src/kernel/kernel.c
 	$(GCC) $(INCLUDES) $(FLAGS) -c ./src/kernel/kernel.c -o ./obj/kernel.o
@@ -27,8 +27,8 @@ all: ./bin/boot.bin ./bin/kernel.bin
 ./obj/mem.o: ./src/kernel/memory/mem.c
 	$(GCC) $(INCLUDES) $(FLAGS) -c ./src/kernel/memory/mem.c -o ./obj/mem.o
 
-./obj/idtloader.o: ./src/kernel/interrupt/idtloader.asm
-	nasm -f elf -g ./src/kernel/interrupt/idtloader.asm -o ./obj/idtloader.o
+./obj/idt.asm.o: ./src/kernel/interrupt/idt.asm
+	nasm -f elf -g ./src/kernel/interrupt/idt.asm -o ./obj/idt.asm.o
 
 ./obj/idt.o: ./src/kernel/interrupt/idt.c
 	$(GCC) $(INCLUDES) $(FLAGS) -c ./src/kernel/interrupt/idt.c -o ./obj/idt.o
--- src/kernel/interrupt/idt.c
+++ src/kernel/interrupt/idt.c
@@ -8,17 +8,63 @@
 #include "mem.h"
 #include "kernel.h"
 
+extern void idt_loader(IDT_R *ptr);
+
 static IDTDescriptor idt[256];
 static IDT_R idtr_descriptor;
 
-extern void idt_loader(IDT_R *ptr);
+static const char messages[][256] = {
+    "Division by Zero (INT 0)\n",
+    "Debug Exception (INT 1)\n",
+    "Non-Maskable Interrupt (NMI - INT 2)\n",
+    "Breakpoint Exception (INT 3)\n",
+    "Overflow (INT 4)\n",
+    "BOUND Range Exceeded (INT 5)\n",
+    "Invalid Opcode (INT 6)\n",
+    "Device Not Available (INT 7)\n",
+    "Double Fault (INT 8)\n",
+    "Coprocessor Segment Overrun (INT 9)\n",
+    "Invalid TSS (INT 10)\n",
+    "Segment Not Present (INT 11)\n",
+    "Stack-Segment Fault (INT 12)\n",
+    "General Protection Fault (INT 13)\n",
+    "Page Fault (INT 14)\n",
+    "Reserved (INT 15)\n",
+    "x87 Floating-Point Exception (INT 16)\n",
+    "Alignment Check (INT 17)\n",
+    "Machine Check (INT 18)\n",
+    "SIMD Floating-Point Exception (INT 19)\n",
+    "Virtualization Exception (INT 20)\n",
+    "Control Protection Exception (INT 21)\n",
+    "Reserved (INT 22)\n",
+    "Reserved (INT 23)\n",
+    "Reserved (INT 24)\n",
+    "Reserved (INT 25)\n",
+    "Reserved (INT 26)\n",
+    "Reserved (INT 27)\n",
+    "Reserved (INT 28)\n",
+    "Reserved (INT 29)\n",
+    "Security Exception (INT 30)\n",
+    "Reserved (INT 31)\n",
+};
 
-void isr_division_by_zero(void)
+/**
+ * @brief Interrupt Service Routine (ISR) for the "Division by Zero"
+ */
+void isr0(void)
 {
-    kprint("Interrupt INT 00h - Divide by 0\n");
+    const char *message = messages[0];
+    kprint(message);
     return;
 };
 
+/**
+ * @brief Sets up an entry in the Interrupt Descriptor Table (IDT).
+ * Configures the specified entry in the IDT with the given vector and ISR (Interrupt Service Routine).
+ * @param vector The interrupt number.
+ * @param isr A pointer to the ISR (Interrupt Service Routine) function.
+ * @return void
+ */
 void idt_set(const int vector, void *isr)
 {
     if (vector < 0 || vector >= 256)
@@ -31,16 +77,23 @@ void idt_set(const int vector, void *isr)
     descriptor->kernel_cs = 0x08;
     descriptor->reserved = 0x00;
     descriptor->attributes = 0b11101110;
-    descriptor->isr_high = (uint32_t)isr >> 16;
+    descriptor->isr_high = ((uint32_t)isr >> 16) & 0xffff;
 };
 
+/**
+ * @brief Initializes the Interrupt Descriptor Table Register (IDTR).
+ * Set up the IDTR descriptor and initializes the first entry
+ * It then passes the IDT address to the assembly routine for further loading.
+ * @return void
+ */
 void idt_init(void)
 {
+    // Set the limit and base address of the IDT descriptor
     idtr_descriptor.limit = (uint16_t)sizeof(IDTDescriptor) * 256 - 1;
     idtr_descriptor.base = (uintptr_t)&idt[0];
-    idt_set(0, isr_division_by_zero);
-
-    // Pass the idt address in idtr_descriptor.base to assembly
+    // Initialize the first IDT entry for the "division by zero" interrupt
+    idt_set(0, isr0);
+    // Pass the IDT address in idtr_descriptor.base to the assembly routine
     idt_loader(&idtr_descriptor);
     return;
 };
