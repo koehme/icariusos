416f6b407e21a105a37a354b15883176100a3de0
Kevin Oehme <Kevin_Oehme@outlook.de>
2023-12-01 21:12:11 +0100

    Start to initialize and configure PIC in x86 assembly
    Introduces assembly code to initialize and configure the Programmable Interrupt Controller (PIC) for handling hardware interrupts in x86 mode. Named constants, such as ICW_1, MODE_8086, PIC_1_CTRL, PIC_1_DATA, PIC_2_CTRL, PIC_2_DATA, IRQ_0, and IRQ_8, have been defined to improve code readability.
    
    The code sets up ICW 1 to enable initialization mode and sends ICW 4. It remaps PIC1 and configures it for 80x86 mode. The constants provide clarity and avoid the use of magic numbers.
    
    This commit is part of the broader effort to establish a foundation for handling hardware interrupts on the x86 architecture.

--- src/bootloader/pm.asm
+++ src/bootloader/pm.asm
@@ -8,6 +8,18 @@ extern kmain
 CODE_SEG equ 0x08
 DATA_SEG equ 0x10
 
+%define ICW_1 0x11				    ; 00010001 binary. Enables initialization mode and we are sending ICW 4
+%define MODE_8086 0b00000001        ; 80x86 mode
+
+%define PIC_1_CTRL 0x20				; Primary PIC control register
+%define PIC_1_DATA 0x21				; Primary PIC data register
+
+%define PIC_2_CTRL 0xA0				; Secondary PIC control register    
+%define PIC_2_DATA 0xA1				; Secondary PIC data register
+    
+%define IRQ_0	0x20				; IRQs 0-7 mapped to use interrupts 0x20-0x27
+%define IRQ_8	0x28				; IRQs 8-15 mapped to use interrupts 0x28-0x36
+
 ;=============================================================================
 ; _start
 ;
@@ -46,6 +58,31 @@ _start:
     in al, 0x92
     or al, 2
     out 0x92, al
+
+;=============================================================================
+; remap_pic1
+;
+; Initialize and configure the Programmable Interrupt Controller (PIC) 
+; for handling hardware interrupts in 80x86 mode.
+;
+; @param None
+;
+; @return None
+;=============================================================================
+.remap_pic1:
+    ; Send ICW 1 --------------------------------------------------------
+    mov al, ICW_1               ; Initialize Control Word 1
+    out PIC_1_CTRL, al          ; Send ICW 1 to the control port of PIC1
+
+    ; Remap PIC1 --------------------------------------------------------
+    mov al, IRQ_0               ; IRQ_0 = 0x20
+                                ; The interrupt vector offset is set to 32 (0x20)
+                                ; to avoid conflicts with CPU-reserved interrupts.
+    out PIC_1_DATA, al          ; Send it to the PIC1
+
+    ; Send ICW 4 - Set x86 mode ----------------------------------------
+    mov al, MODE_8086           ; Bit 0 enables 80x86 mode
+    out PIC_1_DATA, al          ; Send it to the PIC1
 
 ;=============================================================================
 ; run_kernel
