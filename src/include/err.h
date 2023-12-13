7a8aee00f45bdced9169c5480bf825c85cf836a5
Kevin Oehme <Kevin_Oehme@outlook.de>
2023-12-13 21:04:11 +0100

    Update err.h

--- src/include/err.h
+++ src/include/err.h
@@ -12,12 +12,6 @@ typedef enum ErrorCode
 {
     EIO = 5,     // I/O error
     ENOMEM = 12, // Insufficient memory
-    EACCES = 13, // Permission denied
-    ENOENT = 2,  // No such file or directory
-    EINVAL = 22, // Invalid argument
-    EEXIST = 17, // File already exists
-    EPIPE = 32,  // Broken pipe
-    EDEADLK = 45 // Deadlock condition
 } ErrorCode;
 
 #endif
