8b13d750a42fab646328fa227301de7484651e03
Kevin Oehme <Kevin_Oehme@outlook.de>
2023-12-22 21:11:07 +0100

    Start implementing parsing logic

--- src/kernel/fs/pathparser.c
+++ src/kernel/fs/pathparser.c
@@ -18,18 +18,27 @@ identifier          -> 'a' | 'b' | ... | 'z'
 drive               -> 'A' | 'B' | ... | 'Z'
 */
 
+static PathParserNode *path_parser_parse_path(PathParser *self, PathParserRootNode *root_node, PathLexer *path_lexer)
+{
+    path_parser_advance(self, path_lexer);
+
+    while (!parser_match(self, path_lexer, PT_END))
+    {
+    };
+};
+
 /**
  * @brief Parses a path and constructs a PParserRootNode representing the file system path structure.
  * Takes a path as input and constructs a PParserRootNode, which represents
  * the file system structure based on the provided path. The expected format of the path is
  * similar to the example below:
  * @example
- *    PParserRootNode root_node = {
+ *     root_node = {
  *        .drive = 'A', .path = {
- *            PParserPathNode = {
+ *            PathParsrNode = {
  *                .identifier = "bin",
  *                .next = {
- *                    PParserPathNode = {
+ *                    PathParsrNode = {
  *                        .identifier = "cli",
  *                        .next = { 0x0 }
  *                    }
@@ -42,8 +51,9 @@ drive               -> 'A' | 'B' | ... | 'Z'
  * @param path_lexer A pointer to the PLexer instance containing the input to be parsed.
  * @return A pointer to the root node of the parsed file system structure.
  */
-PathParserRootNode *pparser_parse(PathParser *self, PathLexer *path_lexer)
+PathParserRootNode *path_parser_parse(PathParser *self, PathLexer *path_lexer)
 {
     PathParserRootNode *root_node = kcalloc(sizeof(PathParserRootNode));
+    // Pass a double pointer to not modify the original root_node to path_parser_parse_path
     return root_node;
 };
