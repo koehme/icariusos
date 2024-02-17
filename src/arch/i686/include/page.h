658ebbb4fc3ac80ef97c1de8204e59cc15b44013
Kevin <Kevin_Oehme@outlook.de>
2024-02-17 21:15:02 +0100

    Refactoring

--- src/arch/i686/include/page.h
+++ src/arch/i686/include/page.h
@@ -12,29 +12,23 @@
 
 extern void asm_page_enable(void);
 
-typedef uint32_t PageEntry;
-
-/**
- * @brief Representing the translation from virtual address to page.
- */
-typedef struct PageVirtual
+// Translation from virtual address to page dir and page table index
+typedef struct VMemTranslation
 {
     uint32_t pd_index; // Page Directory Index
     uint32_t pt_index; // Page Table Index
-} PageVirtual;
+} VMemTranslation;
 
 typedef enum PageFlags
 {
-    PAGE_PRESENT = 0b00000001,         // Set if the page is present in memory.
-    PAGE_READ_WRITE = 0b00000010,      // If set, the page is writeable; if unset, the page is read-only.
-    PAGE_USER_SUPERVISOR = 0b00000100, // If set, it's a user-mode page; else, it's a supervisor (kernel)-mode page.
-    PAGE_RESERVED = 0b00001000,        // Used by the CPU internally and cannot be trampled.
-    PAGE_ACCESSED = 0b00010000,        // Set if the page has been accessed (set by the CPU).
-    PAGE_DIRTY = 0b00100000,           // Set if the page has been written to (dirty).
-    PAGE_AVAIL = 0b11000000,           // Unused and available for kernel use (3 bits).
-    PAGE_SIZE = 4096,                  // Size of a page in bytes.
-    PAGE_TABLE_ENTRIES = 1024,         // Number of entries in a page table.
-    PAGE_MASK = 0b1111111111,          // Mask to extract the page table index from a virtual address.
+    PAGE_PRESENT = 0b00000001,         // Set if the page is present in memory
+    PAGE_READ_WRITE = 0b00000010,      // If set, the page is writeable; if unset, the page is read-only
+    PAGE_USER_SUPERVISOR = 0b00000100, // If set, it's a user-mode page; else, it's a supervisor (kernel)-mode page
+    PAGE_RESERVED = 0b00001000,        // Used by the CPU internally and cannot be trampled
+    PAGE_ACCESSED = 0b00010000,        // Set if the page has been accessed (set by the CPU)
+    PAGE_DIRTY = 0b00100000,           // Set if the page has been written to (dirty)
+    PAGE_AVAIL = 0b11000000,           // Unused and available for kernel use (3 bits)
+    PAGE_TABLE_INDEX = 0x3FF,          // Mask to extract the page table index from a virtual address
 } PageFlags;
 
 typedef struct PageDirectory
@@ -42,10 +36,8 @@ typedef struct PageDirectory
     uint32_t *directory;
 } PageDirectory;
 
-void page_init_directory(PageDirectory *self, uint8_t flags);
-void page_switch(PageEntry *dir);
-
-PageVirtual virt_address_get_pd_indicies(void *virt_addr);
-int32_t virt_address_map(PageEntry *dir, void *virt_addr, const uint32_t phy_addr);
+void page_init_directory(PageDirectory *self, const uint8_t flags);
+void page_switch(uint32_t *dir);
+int8_t map_frame_to_address(uint32_t *dir, void *vaddr, const uint32_t frame);
 
 #endif
--- src/arch/i686/memory/page.c
+++ src/arch/i686/memory/page.c
@@ -8,114 +8,103 @@
 #include "kernel.h"
 #include <stdbool.h>
 
-extern void asm_page_load(PageEntry *self);
+extern void asm_page_load(uint32_t *self);
 
 PageDirectory kpage_dir = {
     .directory = 0x0,
 };
 
-static PageEntry *ptr_kpage_curr_dir = 0x0;
+static uint32_t *ptr_kpage_curr_dir = 0x0;
 
+// Check if the address is aligned to the page size
 static bool is_addr_aligned(void *addr)
 {
-    const bool is_addr_aligned = ((PageEntry)addr % PAGE_SIZE) == 0;
+    const bool is_addr_aligned = ((uint32_t)addr % 4096) == 0;
     return is_addr_aligned;
 };
 
-/**
- * @brief Allocates memory for the page directory and page tables and initializes them.
- * @param flags The flags to be used for initialization.
- * @return A pointer to the initialized page directory structure.
- */
+// Initialize the page directory with the specified flags
 void page_init_directory(PageDirectory *self, const uint8_t flags)
 {
     // Allocate memory for the 4kb page directory
-    PageEntry *pd = kcalloc(sizeof(PageEntry) * PAGE_TABLE_ENTRIES);
+    uint32_t *pd = kcalloc(sizeof(uint32_t) * 1024);
     size_t pd_offset = 0;
     // Loop through each entry in the page directory
-    for (size_t pd_index = 0; pd_index < PAGE_TABLE_ENTRIES; pd_index++)
+    for (size_t pd_index = 0; pd_index < 1024; pd_index++)
     {
         // Allocate memory for a page table
-        PageEntry *pt_entry = kcalloc(sizeof(PageEntry) * PAGE_TABLE_ENTRIES);
+        uint32_t *pt_entry = kcalloc(sizeof(uint32_t) * 1024);
         // Loop through each entry in the page table
-        for (size_t pt_index = 0; pt_index < PAGE_TABLE_ENTRIES; pt_index++)
+        for (size_t pt_index = 0; pt_index < 1024; pt_index++)
         {
-            const uint32_t phy_addr = pd_offset + (pt_index * PAGE_SIZE);
-            // Set the page table entry to the physical address with flags
+            // Calculate the physical frame address
+            const uint32_t frame = pd_offset + (pt_index * 4096);
+            // Set the page table entry to the physical frame address with flags
             // for example in the first outer iteration with pd_index 0
-            // entry = 0x1002000, inner loop pt_index 0 -> phy_addr = 0x0, 1 => phy_addr = 0x4096 and so on...
-            pt_entry[pt_index] = phy_addr | flags;
+            // pt_entry = 0x1002000, inner loop pt_index 0 -> frame = 0x0, 1 => frame = 0x4096 and so on...
+            pt_entry[pt_index] = frame | flags;
         };
         // Update directory offset for the next iteration
-        pd_offset += (PAGE_SIZE * PAGE_TABLE_ENTRIES);
+        pd_offset += (4096 * 1024);
         // Set the page directory entry to the page table's address with flags
         // after first iteration pd_index is 1 and pd_offset 4194304 or 0x400000
-        pd[pd_index] = (PageEntry)pt_entry | flags;
+        pd[pd_index] = (uint32_t)pt_entry | flags;
     };
+    // Set the global page directory to the allocated page directory
     kpage_dir.directory = pd;
     return;
 };
 
-void page_switch(PageEntry *dir)
+// Switches the current page directory to the one specified
+void page_switch(uint32_t *dir)
 {
+    // Load the given page directory into the CR3 register to switch to the new memory mapping
     asm_page_load(dir);
+    // Update the pointer to the current page directory
     ptr_kpage_curr_dir = dir;
     return;
 };
 
-/**
- * @brief Calculates the Page Directory and Page Table indexes for a given virtual address.
- * Takes a virtual address and computes the corresponding indexes in the Page Directory (PD) and Page Table (PT).
- * @param virt_addr The virtual address for which to calculate indexes.
- * @return A structure containing the Page Directory and Page Table indexes.
- *
- * Assumes a 32-bit paging system with 4 KB page size.
- *
- * Virtual Address Format
- *
- * | Directory Index  | Table Index | Offset  |
- * |------------------|-------------|-------- |
- * |      10 bits     |   10 bits   |  12 bits|
- */
-PageVirtual virt_address_get_pd_indicies(void *virt_addr)
+// Extracts page directory and table indices from a virtual memory address
+static VMemTranslation translate_vaddress(void *vaddr)
 {
-    const bool is_aligned = is_addr_aligned(virt_addr);
+    const bool is_aligned = is_addr_aligned(vaddr);
 
     if (!is_aligned)
     {
-        kpanic("Virtual memory address must be aligned!");
+        kpanic("PageError: Virtual memory address must be aligned!");
     };
+    // * | Directory Index   | Table Index | Offset   |
+    // * |------------------ |-------------|----------|
+    // * |      10 Bits      |   10 Bits   |  12 Bits |
     // Extracts the 10 most significant bits, which represent the index into the Page Directory
-    const size_t pd_index = ((uint32_t)virt_addr >> 22);
+    const uint32_t pd_index = ((uint32_t)vaddr >> 22);
     // Extracts the middle 10 bits, which represent the index into the Page Table
-    const size_t pt_index = ((uint32_t)virt_addr >> 12) & PAGE_MASK;
+    const uint32_t pt_index = ((uint32_t)vaddr >> 12) & PAGE_TABLE_INDEX;
 
-    const PageVirtual pv = {
+    const VMemTranslation translation = {
         .pd_index = pd_index,
         .pt_index = pt_index,
     };
-    return pv;
+    return translation;
 };
 
-/**
- * @brief Maps a virtual address to a physical address in the page tables.
- * @param dir       Pointer to the Page Directory.
- * @param virt_addr Virtual address to be mapped.
- * @param phy_addr  Physical address to be mapped to the virtual address.
- *                  (Includes flags such as PAGE_PRESENT, PAGE_READ_WRITE, etc.)
- * @return 0 on success, -1 on failure.
- */
-int32_t virt_address_map(PageEntry *dir, void *virt_addr, const uint32_t phy_addr)
+int8_t map_frame_to_address(uint32_t *dir, void *vaddr, const uint32_t frame)
 {
-    const bool is_aligned = is_addr_aligned(virt_addr);
+    const bool is_aligned = is_addr_aligned(vaddr);
 
     if (!is_aligned)
     {
+        kprintf("PageError: Virtual memory address must be aligned!\n");
         return -1;
     };
-    const PageVirtual pv = virt_address_get_pd_indicies(virt_addr);
-    const uint32_t pd_entry = dir[pv.pd_index];
+    // Translate the virtual address to page indices
+    const VMemTranslation translation = translate_vaddress(vaddr);
+    // Access the page directory entry corresponding to the virtual address
+    const uint32_t pd_entry = dir[translation.pd_index];
+    // Extract the page table entry for the virtual address
     uint32_t *pt_entry = (uint32_t *)(pd_entry & 0b11111111111111111111000000000000);
-    pt_entry[pv.pt_index] = phy_addr;
+    // Map the provided physical frame to the page table entry
+    pt_entry[translation.pt_index] = frame;
     return 0;
 };
